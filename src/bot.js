26b9cf644c219d04cf887a7e5a151bddc416b297
aqiel14 <72314836+aqiel14@users.noreply.github.com>
2020-12-10 14:32:58 +0700

    catch ???

--- src/bot.js
+++ src/bot.js
@@ -29,6 +29,11 @@ client.on('ready', () => {
 client.on('message', async function(message) {
     if (message.author.bot) return;
 
+    if (message.content == PREFIX) return;
+    if (message.content == PREFIX+PREFIX) return;
+    if (message.content == PREFIX+PREFIX+PREFIX) return;
+
+    
     if (!message.content.startsWith(PREFIX)) return;
     let cmdName = message.content.substring(message.content.indexOf(PREFIX)+1).split(new RegExp(/\s+/)).shift();
     let argsToParse = message.content.substring(message.content.indexOf(' ')+1);
@@ -37,6 +42,8 @@ client.on('message', async function(message) {
     if(client.commands.get(cmdName)) {
         client.commands.get(cmdName)(client,message,argsToParse);
     }
+
+    
     else {
         message.reply("Command doesnt exist!");
     }
